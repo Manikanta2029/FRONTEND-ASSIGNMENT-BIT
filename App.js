0754dc50928ac987089faee91cb33913c2537248
Manikanta2029 <127831434+Manikanta2029@users.noreply.github.com>
2023-12-13 12:18:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import Records from './components/Records'
+// eslint-disable-next-line
+import LoginForm from './components/LoginForm'
+// eslint-disable-next-line
+import Home from './components/Home'
+import './App.css'
+
+const App = () => <Records />
+
+export default App
